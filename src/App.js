a4e83bd63d1a44d32b1ee7a6343e13569091aa5b
Rafael Silva <rafa.dev@hotmail.com>
2019-08-22 20:18:54 -0300

    Config dotenv

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 
 import Header from './components/header';
+import Footer from './components/footer';
 
+require('dotenv').config();
 
 export default function App() {
   return (
-      <BrowserRouter> 
+      <BrowserRouter>
         <Header />
         <Routes />
+        <Footer />
       </BrowserRouter>
   );
 }
-
--- src/services/data/data.service.js
+++ src/services/data/data.service.js
@@ -1,22 +1,19 @@
 import md5 from 'js-md5'
 
-const PUBLIC_KEY = '8609cc64d92ae2230f687909ce922ada';
-const PRIVATE_KEY = '27c7d5f0dc43a13f7716df100a80b6f42a8b7ce8';
-
 export async function getAllCharacters() {
     const timestamp = Number(new Date())
 
-    const characters = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&orderBy=name&limit=50&apikey=${PUBLIC_KEY}&hash=${gerateHash()}`)
+    const characters = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&orderBy=name&limit=50&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${gerateHash()}`)
         .then(response => response.json())
         .then(responseJson => responseJson['data']['results'])
         .catch(err => console.error('Erro ao carregar dados da api: ', err));
-     
+
     return characters;
 }
 
 export async function getSeries (id) {
-  
-    const series = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${id}/series?apikey=${PUBLIC_KEY}&hash=${gerateHash()}`)
+
+    const series = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${id}/series?apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${gerateHash()}`)
         .then(response => response.json())
         .then(responseJson => responseJson['data']['results'])
         .catch(err => console.error('Erro ao carregar dados da api: ', err));
@@ -25,8 +22,8 @@ export async function getSeries (id) {
 }
 
 export async function getDetailsCharacter (id) {
-  
-    const series = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${PUBLIC_KEY}&hash=${gerateHash()}`)
+
+    const series = await fetch(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${gerateHash()}`)
         .then(response => response.json())
         .then(responseJson => responseJson['data']['results'])
         .catch(err => console.error('Erro ao carregar dados da api: ', err));
@@ -36,7 +33,7 @@ export async function getDetailsCharacter (id) {
 
 export async function getCharacterByName (name) {
 
-    const character = fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${name}&apikey=${PUBLIC_KEY}&hash=${gerateHash()}`)
+    const character = fetch(`https://gateway.marvel.com:443/v1/public/characters?name=${name}&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${gerateHash()}`)
         .then(response => response.json())
         .then(responseJson => responseJson['data']['results'])
         .catch(err => console.error('Erro ao carregar dados da api: ', err));
@@ -47,7 +44,7 @@ export async function getCharacterByName (name) {
 export function gerateHash() {
     const timestamp = Number(new Date())
     const hash = md5.create()
-    hash.update(`${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`)
+    hash.update(`${timestamp}${process.env.REACT_APP_PRIVATE_KEY}${process.env.REACT_APP_PUBLIC_KEY}`)
 
     return hash;
 }
